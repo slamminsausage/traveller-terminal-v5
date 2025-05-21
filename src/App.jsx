bdb1a9d07416de082f11e1a13a4d7405f0cc2e1c
slamminsausage <jamesmcadams1313@gmail.com>
2025-05-21 08:50:20 -0500

    Fix HashRouter implementation and CSS paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom"; // Changed from BrowserRouter
+import { HashRouter, Routes, Route } from "react-router-dom";
 import TravellerTerminal from "./components/TravellerTerminal";
 import PoopTerminal from "./components/PoopTerminal";
 import QRCodeGenerator from "./components/QRCodeGenerator";
@@ -7,14 +7,14 @@ import HiddenPage from "./components/HiddenPage";
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<TravellerTerminal />} />
         <Route path="/poop" element={<PoopTerminal />} />
         <Route path="/admin/qr-generator" element={<QRCodeGenerator />} />
         <Route path="/hidden/:pageId" element={<HiddenPage />} />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
 
