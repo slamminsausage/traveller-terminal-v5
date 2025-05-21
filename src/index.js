4382c3a6001a7d6916ff12f2fe3140f246a39e5a
slamminsausage <jamesmcadams1313@gmail.com>
2025-05-21 09:17:38 -0500

    Adjusted fuwnet and a few other minor log changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";  // Make sure App.jsx exists in src/
-import "./styles/global.css"; // Optional: For styling
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
