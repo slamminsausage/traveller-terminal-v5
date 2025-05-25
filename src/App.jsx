f22b72b2687b86b050c65d0ee079aae09ebf5de4
slamminsausage <jamesmcadams1313@gmail.com>
2025-05-24 19:08:58 -0500

    re-added hash routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import TravellerTerminal from "./components/TravellerTerminal";
 import PoopTerminal from "./components/PoopTerminal";
 import RiftjawTerminal from "./components/RiftjawTerminal";
